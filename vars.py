fd795a75087a4acc5e28370200ff85fb3ff00d53
rendersell07-svg <rendersell07@gmail.com>
2026-08-12 15:23:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "34422904"))
 API_HASH = environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8857536514:AAH0WkXHFipYeBX2rj6gTSufN5LW8k1_MXw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8988492520:AAFF9z7uz9PIQzJhy17z95e5sbecT4nFZuI")
 
 OWNER = int(environ.get("OWNER", "5349573682"))
 CREDIT = environ.get("CREDIT", "•Ɱའ★Krΐຮhήส•❤️‍🔥")
